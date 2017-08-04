75a3fb7e80c788eea1b4657f7f75e7856258b8a8
Stephen Hara <sthara216@gmail.com>
2017-08-03 22:10:41 -0400

    add validation around users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803230751) do
+ActiveRecord::Schema.define(version: 20170804020435) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170803230751) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: "Joe", last_name: "Boe", email: "bo@jo.com",
+                      password: "abcd123", password_confirmation: "abcd123")
+  end
+
+  test "should start valid" do
+    assert @user.valid?
+  end
+
+  test "should require name" do
+    @user.first_name = ""
+    assert_not @user.valid?
+
+    @user.last_name = ""
+    @user.first_name = "Joe"
+    assert_not @user.valid?
+  end
+
+  test "should require email" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "should require unique (case insensitive) email" do
+    copy_user = @user.dup
+    copy_user.email = @user.email.upcase
+    @user.save
+    assert_not copy_user.valid?
+  end
+
+  test "should require password" do
+    @user.password = @user.password_confirmation = " " * 10
+    assert_not @user.valid?
+  end
 end
