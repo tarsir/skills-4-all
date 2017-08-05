0437db7259ce7df179501b32035ecfa8c7becbb8
Stephen Hara <sthara216@gmail.com>
2017-08-05 09:17:57 -0400

    add skill uniqueness constraint

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,5 @@
 class Skill < ApplicationRecord
-  validates :description, presence: true
+  validates :description, presence: true, uniqueness: true
   has_many :user_skills
   has_many :users, through: :user_skills
 end
