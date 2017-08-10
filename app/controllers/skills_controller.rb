fbd4feb3630894311579d758f81e194604ea2166
Stephen Hara <sthara216@gmail.com>
2017-08-09 20:11:29 -0400

    add entries to user skill and user skill votes tables with skill

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -18,6 +18,7 @@ class SkillsController < ApiController
     @skill = Skill.new(skill_params)
 
     if @skill.save
+      add_to_user
       render json: @skill, status: :created, location: @skill
     else
       render json: @skill.errors, status: :unprocessable_entity
@@ -33,5 +34,17 @@ class SkillsController < ApiController
     # Only allow a trusted parameter "white list" through.
     def skill_params
       params.require(:skill).permit(:description)
+    end
+
+    def add_to_user
+      voter = params[:voter], receiver = params[:receiver]
+      pp @skill
+      unless UserSkill.find_by user_id: receiver, skill_id: @skill.id
+        UserSkill.create(user_id: receiver, skill_id: @skill.id)
+      end
+
+      unless UserSkillVote.find_by voter_id: voter, receiver_id: receiver, skill_id: @skill.id
+        UserSkillVote.create(voter_id: voter, receiver_id: receiver, skill_id: @skill.id)
+      end
     end
 end
