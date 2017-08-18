da89ae429d18acc5edffe0e9ee29f6efb25d461c
Stephen Hara <sthara216@gmail.com>
2017-08-18 01:43:50 -0400

    add HTML fallback to rails routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
     ActiveAdmin.routes(self)
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
 end
